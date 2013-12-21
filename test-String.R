cd5ff7e03b46020c3838dcfa9d7256c9fb473266
Romain Francois <romain@r-enthusiasts.com>
2013-12-21 15:14:00 +0100

    merge tests about strings

--- test-String.R
+++ test-String.R
@@ -20,4 +20,10 @@ test_that( "String comparison works", {
   )
   expect_equal( res, target )
 })
-                                    
+             
+test_that( "wstring are supported", {
+  expect_equal( CharacterVector_wstring(), c("foobar", "foobar" ) )
+  expect_equal( wstring_return(), "foo" )
+  expect_equal( wstring_param( "foo", "bar" ), "foobar" )
+  expect_equal( wrap_vector_wstring( ), c("foo", "bar" ) )
+})
