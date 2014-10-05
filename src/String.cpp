fcd49fb13c48fb0fff09b63f5752a1d517085012
Romain François <romain@r-enthusiasts.com>
2014-10-05 14:57:44 +0200

    rm some String tests

--- src/String.cpp
+++ src/String.cpp
@@ -3,49 +3,6 @@ using namespace Rcpp ;
 
 
 // [[Rcpp::export]]
-String String_replace_all( String z, String x, String y){
-    z.replace_all( x, y ) ;
-    return z ;
-}
-
-// [[Rcpp::export]]
-String String_replace_first( String z, String x, String y){
-    z.replace_first( x, y ) ;
-    return z ;
-}
-// [[Rcpp::export]]
-String String_replace_last( String z, String x, String y){
-    z.replace_last( x, y ) ;
-    return z ;
-}
-
-class StringConv{
-public:
-    typedef String result_type ;
-    StringConv( CharacterVector old_, CharacterVector new__): 
-        nr(old_.size()), old(old_), new_(new__){}
-    
-    String operator()(String text) const {
-        for( int i=0; i<nr; i++){
-            text.replace_all( old[i], new_[i] ) ;
-        }     
-        return text ;
-    }
-    
-private:
-    int nr ;
-    CharacterVector old ;
-    CharacterVector new_ ;
-} ;
-
-// [[Rcpp::export]]
-CharacterVector test_sapply_string( CharacterVector text, CharacterVector old , CharacterVector new_){
-    StringConv conv( old, new_ );
-    CharacterVector res = sapply( text, conv  ) ;
-    return res ;
-}  
-
-// [[Rcpp::export]]
 List test_compare_Strings( String aa, String bb ){
     return List::create(
         _["a  < b" ] = aa < bb, 
--- tests/testthat/test-String.R
+++ tests/testthat/test-String.R
@@ -1,15 +1,5 @@
 context( "String" )
 
-
-test_that( "replace functions work", {
-  expect_equal( String_replace_all("foobar", "o", "*"), "f**bar")
-  expect_equal( String_replace_first("foobar", "o", "*"), "f*obar")
-  expect_equal( String_replace_last("foobar", "o", "*"), "fo*bar")
-
-  res <- test_sapply_string( "foobar", c("o", "a" ), c("*", "!" ) )
-  expect_equal( res, "f**b!r" )    
-})
-
 test_that( "String comparison works", {
   res <- test_compare_Strings( "aaa", "aab" )
   target <- list( 
